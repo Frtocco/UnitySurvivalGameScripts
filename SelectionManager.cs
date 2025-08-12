0b6c6725ea1183550760513024d1070be97b2625
Francisco <frtocco@uade.edu.ar>
2025-08-12 09:04:00 -0300

    Selection manager icons

--- SelectionManager.cs
+++ SelectionManager.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Unity.VisualScripting;
+using Microsoft.Unity.VisualStudio.Editor;
 
 public class SelectionManager : MonoBehaviour
 {
     public GameObject interaction_Info_UI;
     TMP_Text interaction_text;
     public float maxDistance = 3f;
+
+    public Image centerDotImage;
+    public Image handIcon;
 
     private void Start()
     {
@@ -19,26 +23,26 @@ public class SelectionManager : MonoBehaviour
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-    
+
         if (Physics.Raycast(ray, out hit, maxDistance))
         {
             var selectionTransform = hit.transform;
             var interactable = selectionTransform.GetComponent<InteractableObject>();
-    
+
             if (interactable != null)
             {
                 interaction_text.text = interactable.GetItemName();
                 interaction_Info_UI.SetActive(true);
-    
+
                 if (Input.GetKeyDown(KeyCode.Mouse0))
                 {
                     interactable.OnInteract();
                 }
-    
+
                 return;
             }
         }
-    
+
         interaction_Info_UI.SetActive(false);
     }
 
